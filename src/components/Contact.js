f8ee781a7c44524dfe6155809eb3f1a3b133f008
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-17 12:01:14 -0400

    Added components folder, created contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Kenneth Olsen</h4>
+        <ul>
+          <li>Email: olsenkenneth1986@gmail.com</li>
+          <li>Phone: 313-247-0516</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
