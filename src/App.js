ac55cba8e870594e953981e45a749318bf1a8f82
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-20 13:50:03 -0400

    Added bootstrap, styled Header and Contact components with bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Header branding="Contact Manager" />
-        <Contact name="Kenneth Olsen"
-        email="olsenkenneth1986@gmail.com"
-        phone="313-247-0516"
-         />
+        <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+                <Contact
+                    name="Kenneth Olsen"
+                    email="olsenkenneth1986@gmail.com"
+                    phone="313-247-0516"
+                />
 
-<Contact name="Turd Bomber"
-        email="bringthebrown@gmail.com"
-        phone="313-247-brown"
-         />
-      </div>
+                <Contact
+                    name="Turd Bomber"
+                    email="bringthebrown@gmail.com"
+                    phone="313-247-brown"
+                />
+            </div>
+        </div>
     );
   }
 }
